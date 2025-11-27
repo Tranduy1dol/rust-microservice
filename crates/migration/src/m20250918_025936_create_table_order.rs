aa82e7d6ff614a5f5ccce70e1369dc16a4d7ec9b
Tranduy1dol <tmd.id2004@gmail.com>
2025-11-27 17:39:10 +0700

    feat: add Dockerfile for CD

--- crates/migration/src/m20250918_025936_create_table_order.rs
+++ crates/migration/src/m20250918_025936_create_table_order.rs
@@ -36,14 +36,14 @@ impl MigrationTrait for Migration {
                     )
                     .col(ColumnDef::new(Order::UserId).big_integer().not_null())
                     .col(
-                        ColumnDef::new(Order::CreatedAt)
+                        ColumnDef::new(Order::OrderDate)
                             .big_integer()
                             .not_null()
                             .default(Expr::cust(
                                 "CAST(EXTRACT(EPOCH FROM NOW()) * 1000 AS BIGINT)",
                             )),
                     )
-                    .col(ColumnDef::new(Order::Status).enumeration(
+                    .col(ColumnDef::new(Order::OrderStatus).enumeration(
                         OrderStatusEnum::OrderStatus,
                         [
                             OrderStatusEnum::Placed,
@@ -89,8 +89,8 @@ enum Order {
     Table,
     Id,
     UserId,
-    CreatedAt,
-    Status,
+    OrderDate,
+    OrderStatus,
     TotalAmount,
 }
 
